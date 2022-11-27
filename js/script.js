f05d6a4a7694878f1186aa06676ec2c71b55d6f6
Olga <olg200919@ya.ru>
2022-11-27 14:46:09 +0300

    Задача_3

--- js/script.js
+++ js/script.js
@@ -9,7 +9,39 @@ for (let i = 0; i < 11; i++) {
     }
   }*/
 
-/*Задача 2*/
+/*Задача 2
 const arr = [1, 2, 3, 4, 5, 6, 7];
 arr2 = [].concat(arr.slice(0, 3), arr.slice(5, 7));
-console.log(arr2);
+console.log(arr2);*/
+
+/*Задача 3*/
+const array = [];
+for (let i = 0; i < 5; i++) {
+    array[i] = Math.floor(Math.random() * 9);
+}
+console.log(array);
+
+let sum = 0;
+for (let i = 0; i < array.length; i++) {
+    sum = sum + array[i];
+}
+console.log(`Сумма массива ${sum}`);
+
+let minNumber = array[0];
+for (let i = 0; i < array.length; i++) {
+    if (array[i] < minNumber) {
+        minNumber = array[i];
+    }
+}
+console.log(minNumber)
+
+let findNumber = 0;
+for (let i = 0; i < array.length; i++) {
+    if (array[i] === 3) {
+        console.log(`В массиве есть число 3`)
+        break;
+    }
+}
+
+
+
